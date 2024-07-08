0fde84340158b390f8de4155da1067722c1e0ef4
Tanjung <tanjung.tanjung.1466@gmail.com>
2024-07-09 02:39:34 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-purple": "linear-gradient(135deg, #503F76 100%, #5E4EA4 80%)",
       },
+      colors:{
+        'primary-purple': '#503F76',
+        'primary-blue': '#6C5DD2',
+        'primary-orange': '#FF9F24',
+        'primary-cream': '#FFCDB5',
+        'primary-white' : "#F6E8DF"
+      }
     },
   },
   plugins: [],
